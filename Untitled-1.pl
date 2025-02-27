8a796eeba26d2a3259b1263f46521930c2d99d7c
WILLIAM-863 <william.sanchez@utp.edu.co>
2025-02-27 00:46:32 -0500

    Add initial implementation of travel connections and cost calculation

---
+++ Untitled-1.pl
@@ -0,0 +1,20 @@
+conexion(vancouver,edmonton,16).
+conexion(vancouver,calgary,13).
+conexion(edmonton,saskatoon,12).
+conexion(saskatoon,calgary,9).
+conexion(saskatoon,winnipeg,20).
+conexion(calgary,edmonton,4).
+conexion(calgary,regina,14).
+conexion(regina,saskatoon,7).
+conexion(regina,winnipeg,4).
+
+
+ruta(X,Y):-conexion(X,Y,_).
+
+rutacosto(X,Y,Z):-conexion(X,Y,Z).
+%tienearistas(K):-conexion(K,,).
+
+costoviaje(Inicio,Destino,Intermedio,Costo):-conexion(Inicio,Intermedio,C1),conexion(Intermedio,Destino,C2), Costo is C1+C2.
+
+%camino(X,Y):-conexion(X,Y).
+%camino(X,Y):-conexion(X,Z),conexion(Z,Y).
